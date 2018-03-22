df088033f8f9da777a46107ec39d1956d802d9fc
Ed Brook <edbrook@gmail.com>
2018-03-22 18:39:12 +0000

    Add note to change dummy password

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 config = {
     'db': {
-        'user': 'employme',
-        'password': 'OfkDT7@Xauc-VVMG0Ds=',
+        'user': 'employme',                 # Update with real username
+        'password': 'OfkDT7@Xauc-VVMG0Ds=', # !!! CHANGE THIS !!!
         'host': '127.0.0.1',
         'database': 'employme'
     },
